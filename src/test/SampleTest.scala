280e01e7d3308a3904c030bf78dea96416e0a4e4
Tianyu Cheng <skysource.tony@gmail.com>
2014-10-31 10:11:57 -0500

    unit testing with intuitive matchers

--- src/test/SampleTest.scala
+++ src/test/SampleTest.scala
@@ -1,10 +1,10 @@
 import org.scalatest._
 
-class TestHelloWorld extends FlatSpec {
+class TestHelloWorld extends FlatSpec with Matchers {
 
   "Hello World" should "print hello world greeting" in {
     val result = "Hello, World!"
-    assert(result == "Hello, World!")
+    result should be ("Hello, World!")
   }
 
 }
